82dffd5ea4118ee82d563c16e14bafdef27e34a4
Alejandro Acevedo <a.acevedo.aracena@gmail.com>
2023-06-02 13:12:36 -0400

    scripts

--- to_aws.sh
+++ to_aws.sh
@@ -4,13 +4,18 @@ echo "Starting ray up..."
 ray up aws-ray-cluster.yml --yes
 
 echo "Starting ray rsync-up..."
-ray rsync-up aws-ray-cluster.yml graph.graphml graph.graphml
+ray rsync-up aws-ray-cluster.yml data/tirosinemia.csv tirosinemia.csv 
+ray rsync-up aws-ray-cluster.yml data/tirosinemia_italia.csv tirosinemia_italia.csv
+
+
+ray rsync-up aws-ray-cluster.yml requirements.txt requirements.txt
+ray rsync-up aws-ray-cluster.yml update_base_environment.sh update_base_environment.sh
+
 
 echo "Starting ray submit..."
-ray submit aws-ray-cluster.yml centralities.py
+ray submit aws-ray-cluster.yml to_ray_cluster.py
 
 echo "All AWS tasks finished."
 
 echo "downloading AWS results and logs"
-ray rsync-down aws-ray-cluster.yml /home/ubuntu/centralities.log aws-downloads/centralities.log
-ray rsync-down aws-ray-cluster.yml /home/ubuntu/centralities.pickle aws-downloads/centralities.pickle
+ray rsync-down aws-ray-cluster.yml /home/ubuntu/1a_hello.pickle aws-downloads/1a_hello_aws.pickle
